b033698885d62c89a9d543c3f052a4376a5570d9
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-17 10:12:39 -0400

    Add validations to ReceiptRecords model

--- app/models/receipt_record.rb
+++ app/models/receipt_record.rb
@@ -1,2 +1,12 @@
 class ReceiptRecord < ApplicationRecord
+    validates :trans_date, presence: true
+    validates :provider, presence: true
+    validates :amount, presence: true
+    validates :payment_method, presence: true
+    validates :reimbursed, presence: true, inclusion: {
+        in: ['Yes', 'No', 'N/A'],
+        message: 'is not a valid option'
+    }
+
+    belongs_to :user
 end
