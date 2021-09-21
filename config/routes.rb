616e4d1a4250e14c4349fa61cbd04167f6d214c2
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 10:01:54 -0400

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
     get "/profile", to: "users#show"
     post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
 end
