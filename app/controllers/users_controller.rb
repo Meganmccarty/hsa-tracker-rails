d6a0f0cf8af20699eb3f8697f849f2d81a3127dc
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 09:56:38 -0400

    Create show and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
     def show
+        user = User.find_by(email: params[:email])
+        render json: user
     end
 
     def create
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
