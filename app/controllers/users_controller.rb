d9e5692bd94762c7f2cf584104c32dffe08d6cce
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 09:59:16 -0400

    Add method for error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :authorize, only: [:show]
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def show
         user = User.find_by(email: params[:email])
@@ -22,6 +23,10 @@ class UsersController < ApplicationController
 
     def authorize
         render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end
