0d85da389fbc02a9ee9b680ec48a78d999c06aad
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 09:34:45 -0400

    Add methods for error handling

--- app/controllers/receipt_records_controller.rb
+++ app/controllers/receipt_records_controller.rb
@@ -1,4 +1,6 @@
 class ReceiptRecordsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFonud, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
     end
@@ -10,6 +12,16 @@ class ReceiptRecordsController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: 'Receipt record not found' }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end
