290e9eaddc4d7a61dccf2e37769f3dc392e7d7f0
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 09:53:34 -0400

    Create routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
     resources :receipt_records, only: [:index, :create, :update, :destroy]
+
+    get "/profile", to: "users#show"
+    get "/signup", to: "users#create"
 end
