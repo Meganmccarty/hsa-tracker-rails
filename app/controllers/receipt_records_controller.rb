8b559570bbc9d7959e22a8b505c27963b345aee0
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 09:45:25 -0400

    Create index and create methods

--- app/controllers/receipt_records_controller.rb
+++ app/controllers/receipt_records_controller.rb
@@ -3,9 +3,19 @@ class ReceiptRecordsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
+        receipt_records = ReceiptRecord.all
+        render json: receipt_records
     end
 
     def create
+        user = User.find_by(id: session[:user_id])
+
+        receipt_record = ReceiptRecord.new(receipt_record_params)
+        receipt_record.user_id = user.id
+        receipt_record.attach(params[:images])
+        receipt_record.save
+
+        render json: receipt_record, status: :created
     end
 
     def update
@@ -15,6 +25,12 @@ class ReceiptRecordsController < ApplicationController
     end
 
     private
+
+    def receipt_record_params
+        params.permit(:trans_date, :category, :provider, :description, :qualified_exp,
+            :amount, :payment_method, :reimbursed, :notes, :hsa_trans_id, images: []
+        )
+    end
 
     def render_not_found_response
         render json: { error: 'Receipt record not found' }, status: :not_found
