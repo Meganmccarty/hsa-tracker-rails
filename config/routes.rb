3e8d086485cfd2dcc751dfacac86e102625b9f93
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-21 10:00:07 -0400

    Fix /signup route to be post, not get

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     resources :receipt_records, only: [:index, :create, :update, :destroy]
 
     get "/profile", to: "users#show"
-    get "/signup", to: "users#create"
+    post "/signup", to: "users#create"
 end
