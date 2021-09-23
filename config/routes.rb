8ebcd81b0aa78c4bc49536f89d0e2c22b19d58ac
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 15:00:19 -0400

    Add show route for receipt records

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     resources :receipt_records, only: [:create, :update, :destroy]
 
     get "/receipt-records", to: "receipt_records#index"
+    get "/receipt-records/:id", to: "receipt_records#show"
 
     get "/profile", to: "users#show"
     post "/signup", to: "users#create"
