382945a9be29c15c598630604c4521ccbed7b5e1
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 12:09:35 -0400

    Add get route for receipt records (use - instead of _)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    resources :receipt_records, only: [:index, :create, :update, :destroy]
+    resources :receipt_records, only: [:create, :update, :destroy]
+
+    get "/receipt-records", to: "receipt_records#index"
 
     get "/profile", to: "users#show"
     post "/signup", to: "users#create"
