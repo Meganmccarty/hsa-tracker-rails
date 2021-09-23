85b260a8b0b82663904111793e8d0fb0ccf8fc41
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-23 09:10:53 -0400

    Include cookies and method for generating CSRF tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+    before_action :set_csrf_cookie
+
+    def set_csrf_cookie
+        if Rails.env == "development"
+            cookies["CSRF-TOKEN"] = {
+                value: form_authenticity_token
+            }
+        elsif Rails.env == "production"
+            cookies["CSRF-TOKEN"] = {
+                value: form_authenticity_token,
+                secure: true,
+                same_site: :strict,
+                domain: 'life-lister.herokuapp.com'
+            }
+        end
+    end
 end
