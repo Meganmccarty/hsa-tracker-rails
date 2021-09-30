f181402b6212e6b3523efaa0afdfdfae4acb8824
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-30 13:26:01 -0400

    Add devise migrations for user model

---
+++ db/migrate/20210930151544_change_jti_users.rb
@@ -0,0 +1,6 @@
+class ChangeJtiUsers < ActiveRecord::Migration[6.1]
+    def change
+        change_column_null :users, :jti, false
+        add_index :users, :jti, unique: true
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_153741) do
+ActiveRecord::Schema.define(version: 2021_09_30_151544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,9 +64,21 @@ ActiveRecord::Schema.define(version: 2021_09_17_153741) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "jti", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
