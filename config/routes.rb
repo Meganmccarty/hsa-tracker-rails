fb18944372c6ef140cd911acc560780127b80728
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-30 13:28:04 -0400

    Add routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+    devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+    },
+    controllers: {
+        sessions: 'sessions',
+        registrations: 'registrations'
+    },
+    defaults: {
+        format: :json
+    }
+    
     get "/receipt-records", to: "receipt_records#index"
     get "/receipt-records/:id", to: "receipt_records#show"
     post "/receipt-records", to: "receipt_records#create"
@@ -6,7 +19,7 @@ Rails.application.routes.draw do
     delete "/receipt-records/:id", to: "receipt_records#destroy"
 
     get "/profile", to: "users#show"
-    post "/signup", to: "users#create"
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
+    # post "/signup", to: "users#create"
+    # post "/login", to: "sessions#create"
+    # delete "/logout", to: "sessions#destroy"
 end
