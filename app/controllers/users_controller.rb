14d40016db0bbc8f6fcff836cab70a94458baa70
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-30 13:36:12 -0400

    Rewrite users controller for devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,13 @@
 class UsersController < ApplicationController
-    before_action :authorize, only: [:show]
+    before_action :authenticate_user!
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def show
-        user = User.find_by(id: session[:user_id])
-        render json: user, include: ['receipt_records']
-    end
-
-    def create
-        user = User.create!(user_params)
-        if user.valid?
-            session[:user_id] = user.id
-            render json: user, status: :created
-        end
+        user = current_user
+        render json: user
     end
 
     private
-
-    def user_params
-        params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
-
-    def authorize
-        render json: { errors: "Not authorized" }, status: :unauthorized unless session.include? :user_id
-    end
 
     def render_unprocessable_entity_response(invalid)
         render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
