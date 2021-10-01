dc6c5fbdf6f0012739440738bd13f3f4b2072aa6
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-01 10:27:50 -0400

    Configure active mailer and email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,22 @@ Rails.application.configure do
     config.active_storage.replace_on_assign_to_many = false
 
     # Don't care if the mailer can't send.
-    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.perform_caching = true
 
-    config.action_mailer.perform_caching = false
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
+
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        user_name: ENV["EMAIL_ACCOUNT"],
+        password: ENV["EMAIL_PASSWORD"],
+        authentication: "plain",
+        enable_starttls_auto: true
+    }
 
     # Print deprecation notices to the Rails logger.
     config.active_support.deprecation = :log
