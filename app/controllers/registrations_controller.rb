9d8f5cf2349b7647827f4fc6322387110cbc67fd
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-01 17:46:39 -0400

    Return current user upon signup, put errors in array

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,15 @@ class RegistrationsController < Devise::RegistrationsController
 
     def respond_with(resource, _opts = {})
         if resource.persisted?
-            render json: {
-                status: { code: 200, message: 'Signed up sucessfully' }
-            }, status: :ok
+            if current_user
+                render json: {
+                    user: current_user,
+                    status: { code: 200, message: 'Signed up sucessfully' }
+                }, status: :ok
+            end
         else
             render json: {
-                status: { code: 422, errors: "Sign up failed. #{resource.errors.full_messages.to_sentence}" }
+                status: { code: 422, errors: ["Sign up failed. #{resource.errors.full_messages.to_sentence}"] }
             }, status: :unprocessable_entity
         end
     end
