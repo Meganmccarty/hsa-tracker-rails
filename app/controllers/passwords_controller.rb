3eadf3bb95135440c25c7220078ec3f0e605b31f
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-01 12:39:33 -0400

    Add update method

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,5 @@
 class PasswordsController < ApplicationController
+    before_action :authenticate_user!, only: [:update]
     
     def forgot
         user = User.find_by(email: params[:email])
@@ -22,6 +23,26 @@ class PasswordsController < ApplicationController
         else
             render json: { errors: "Link not valid or expired. Try generating a new link." }, status: :not_found
         end
+    end
+
+    def update
+        user = current_user
+        old_password = params[:old_password]
+        if user&.valid_password?(old_password)
+            if user.update!(password_params)
+                render json: { user: user, message: "Password successfully updated" }, status: 200
+            else
+                render json: { message: "New passwords do not match" }, status: :unprocessable_entity
+            end
+        else
+            render json: { message: "Old password is incorrect" }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def password_params
+        params.permit(:password, :password_confirmation)
     end
 
 end
