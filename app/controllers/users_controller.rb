5defa0b55b5242fe6ae1c2f0457f0bf284d91a7f
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-01 12:39:20 -0400

    Add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,21 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    def update
+        user = current_user
+        if user
+            user.update!(user_params)
+            render json: { user: user, message: "Profile successfully updated" }, status: 200
+        else
+            render json: { errors: "Not authorized" }, status: :unauthorized
+        end
+    end
+
     private
+
+    def user_params
+        params.permit(:first_name, :last_name, :email)
+    end
 
     def render_unprocessable_entity_response(invalid)
         render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
