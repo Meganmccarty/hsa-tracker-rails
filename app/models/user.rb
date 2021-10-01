16ea9e73753c25afa24b6a1f714956e3c608b703
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-01 10:29:15 -0400

    Add methods to user model for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,27 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
 
     has_many :receipt_records, -> { order(trans_date: :desc) }
+
+    def send_password_reset
+        self.reset_password_token = generate_base64_token
+        self.reset_password_sent_at = Time.zone.now
+        save!
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    def reset_password_token_valid?
+        (self.reset_password_sent_at + 1.hour) > Time.zone.now
+    end
+
+    def reset_password(password)
+        self.reset_password_token = nil
+        self.password = password
+        save!
+    end
+
+    private
+
+    def generate_base64_token
+        SecureRandom.urlsafe_base64
+    end
 end
