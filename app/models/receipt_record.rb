256bfc7f7d31a03842f93026ee973f5f664851ab
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-02 14:22:30 -0400

    Remove some validations (no longer needed, frontend simplified)

--- app/models/receipt_record.rb
+++ app/models/receipt_record.rb
@@ -1,16 +1,8 @@
 class ReceiptRecord < ApplicationRecord
     validates :trans_date, presence: true
     validates :provider, presence: true
-    validates :qualified_exp, inclusion: {
-        in: [true, false],
-        message: 'is not a valid option'
-    }
     validates :amount, presence: true
     validates :payment_method, presence: true
-    validates :reimbursed, presence: true, inclusion: {
-        in: ['Yes', 'No', 'N/A'],
-        message: 'is not a valid option'
-    }
 
     belongs_to :user
 
