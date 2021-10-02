640a094e5fbcf9de6b6ddc202d68bf3e6d3b3c06
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-02 14:34:29 -0400

    Add validations back in, actually still need them

--- app/models/receipt_record.rb
+++ app/models/receipt_record.rb
@@ -1,8 +1,16 @@
 class ReceiptRecord < ApplicationRecord
     validates :trans_date, presence: true
     validates :provider, presence: true
+    validates :qualified_exp, presence: true, inclusion: {
+        in: ['Yes', 'No'],
+        message: 'is not a valid option'
+    }
     validates :amount, presence: true
     validates :payment_method, presence: true
+    validates :reimbursed, presence: true, inclusion: {
+        in: ['Yes', 'No', 'N/A'],
+        message: 'is not a valid option'
+    }
 
     belongs_to :user
 
